624d1204ff3bbd1a509ddfcc2a0463f823040f03
Mia Wood <111761589+CesarPR30@users.noreply.github.com>
2026-03-01 12:44:09 -0500

    Delete eval_local.py

--- eval_local.py
+++
@@ -1,8 +0,0 @@
-from evaluation import evaluate_agent_scalar
-from solucion import Agent
-
-seeds = [0] #list(range(50))
-
-agent = Agent()
-res = evaluate_agent_scalar(agent, seeds, size=4, max_steps=5000)
-print(res)
